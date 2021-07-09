afc0334e1a158882cd0452167c0513da44271e14
Wes <averagewizard13@gmail.com>
2021-07-08 20:03:55 -0600

    Add basic sphere render

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const point3 &center, double radius, const ray &r) {
+  vec3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius*radius;
+  auto discriminant = b*b - 4*a*c;
+  return (discriminant > 0);
+}
+
 color ray_color(const ray &r) {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+    return color(1, 0, 0);
+  }
+
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
@@ -34,9 +47,9 @@ int main() {
   for (int y = image_height-1; y >= 0; --y) {
     std::cerr << "\rScanlines remaining: " << y << ' ' << std::flush;
     for (int x = 0; x < image_width; ++x) {
-      auto u = double(x) / (image_width-1);
-      auto v = double(y) / (image_height-1);
-      ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+      auto px = double(x) / (image_width-1);
+      auto py = double(y) / (image_height-1);
+      ray r(origin, lower_left_corner + px*horizontal + py*vertical - origin);
       color pixel_color = ray_color(r);
       write_color(std::cout, pixel_color);
     }
