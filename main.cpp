b13b0fc67e26dcde90b088b03aea78aefd28348c
Wes <averagewizard13@gmail.com>
2021-07-10 12:32:41 -0600

    Add camera positioning and FOV

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ color ray_color(const ray &r, const hittable &world, int depth) {
 int main() {
   // -- Image
   const auto aspect_ratio = 16.0 / 9.0;
-  const int image_width = 400;
+  const int image_width = 900;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
   const int samples_per_pixel = 100;
   const int max_depth = 50;
@@ -51,7 +51,7 @@ int main() {
   world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
   // -- Camera
-  camera cam;
+  camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 50.0, aspect_ratio);
 
   // -- Render
   // Header
