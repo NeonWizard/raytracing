28deabc32497c944bdaa6a78443302778ef6deda
Wes <averagewizard13@gmail.com>
2021-07-09 18:01:02 -0600

    Add antialiasing

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 color ray_color(const ray &r, const hittable &world) {
   hit_record rec;
@@ -22,6 +23,7 @@ int main() {
   const auto aspect_ratio = 16.0 / 9.0;
   const int image_width = 400;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
+  const int samples_per_pixel = 100;
 
   // -- World
   hittable_list world;
@@ -29,14 +31,7 @@ int main() {
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
   // -- Camera
-  auto viewport_height = 2.0;
-  auto viewport_width = aspect_ratio * viewport_height;
-  auto focal_length = 1.0;
-
-  auto origin = point3(0, 0, 0);
-  auto horizontal = vec3(viewport_width, 0, 0);
-  auto vertical = vec3(0, viewport_height, 0);
-  auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+  camera cam;
 
   // -- Render
   // Header
@@ -46,11 +41,14 @@ int main() {
   for (int y = image_height-1; y >= 0; --y) {
     std::cerr << "\rScanlines remaining: " << y << ' ' << std::flush;
     for (int x = 0; x < image_width; ++x) {
-      auto px = double(x) / (image_width-1);
-      auto py = double(y) / (image_height-1);
-      ray r(origin, lower_left_corner + px*horizontal + py*vertical - origin);
-      color pixel_color = ray_color(r, world);
-      write_color(std::cout, pixel_color);
+      color pixel_color(0, 0, 0);
+      for (int s = 0; s < samples_per_pixel; ++s) {
+        auto px = (x + random_double()) / (image_width-1);
+        auto py = (y + random_double()) / (image_height-1);
+        ray r = cam.get_ray(px, py);
+        pixel_color += ray_color(r, world);
+      }
+      write_color(std::cout, pixel_color, samples_per_pixel);
     }
   }
 
