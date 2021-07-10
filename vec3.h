088fe783d2f93efcc7276d1cfc0a7c7aaa859425
Wes <averagewizard13@gmail.com>
2021-07-09 21:55:24 -0600

    Improvements

--- vec3.h
+++ vec3.h
@@ -106,12 +106,30 @@ inline vec3 unit_vector(vec3 v) {
   return v / v.length();
 }
 
+// Diffusion #1
+// point3 target = rec.p + rec.normal + random_in_unit_sphere();
 vec3 random_in_unit_sphere() {
   while (true) {
     auto p = vec3::random(-1, 1);
     if (p.length_squared() >= 1) continue;
     return p;
   }
+}
+
+// Diffusion #2
+// point3 target = rec.p + rec.normal + random_unit_vector();
+vec3 random_unit_vector() {
+  return unit_vector(random_in_unit_sphere());
+}
+
+// Diffusion #3
+// point3 target = rec.p + random_in_hemisphere(rec.normal);
+vec3 random_in_hemisphere(const vec3 &normal) {
+  vec3 in_unit_sphere = random_in_unit_sphere();
+  if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+    return in_unit_sphere;
+  else
+    return -in_unit_sphere;
 }
 
 #endif
