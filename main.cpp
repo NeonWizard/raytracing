42f6b025c0ef44b2833e697af6bfa8160f0cc955
Wes <averagewizard13@gmail.com>
2021-07-09 21:33:07 -0600

    Basic diffusion

--- main.cpp
+++ main.cpp
@@ -6,10 +6,17 @@
 #include "sphere.h"
 #include "camera.h"
 
-color ray_color(const ray &r, const hittable &world) {
+color ray_color(const ray &r, const hittable &world, int depth) {
   hit_record rec;
+
+  // If we've exceeded the ray bounce limit, no more light is gathered
+  if (depth <= 0) {
+    return color(0, 0, 0);
+  }
+
   if (world.hit(r, 0, infinity, rec)) {
-    return 0.5 * (rec.normal + color(1, 1, 1));
+    point3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
   }
 
   // -- Sky
@@ -24,6 +31,7 @@ int main() {
   const int image_width = 400;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
   const int samples_per_pixel = 100;
+  const int max_depth = 50;
 
   // -- World
   hittable_list world;
@@ -46,7 +54,7 @@ int main() {
         auto px = (x + random_double()) / (image_width-1);
         auto py = (y + random_double()) / (image_height-1);
         ray r = cam.get_ray(px, py);
-        pixel_color += ray_color(r, world);
+        pixel_color += ray_color(r, world, max_depth);
       }
       write_color(std::cout, pixel_color, samples_per_pixel);
     }
