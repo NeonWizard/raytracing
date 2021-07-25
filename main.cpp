6ceb38f391e2ed7b3df0db56f995744d6cd4ea52
Wes <averagewizard13@gmail.com>
2021-07-25 00:05:00 -0600

    Add probability density function

--- main.cpp
+++ main.cpp
@@ -30,12 +30,14 @@ color ray_color(const ray &r, const color &background, const hittable &world, in
   ray scattered;
   color attenuation;
   color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+  double pdf;
+  color albedo;
 
-  if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+  if (!rec.mat_ptr->scatter(r, rec, albedo, scattered, pdf)) {
     return emitted;
   }
 
-  return emitted + attenuation * ray_color(scattered, background, world, depth-1);
+  return emitted + albedo * rec.mat_ptr->scattering_pdf(r, rec, scattered) * ray_color(scattered, background, world, depth-1) / pdf;
 }
 
 hittable_list random_scene() {
@@ -266,7 +268,7 @@ int main() {
   auto aperture = 0.0;
   color background(0, 0, 0);
 
-  switch (0) {
+  switch (6) {
     case 1:
       world = random_scene();
       background = color(0.70, 0.80, 1.00);
@@ -311,15 +313,15 @@ int main() {
       break;
 
     case 6:
-        world = cornell_box();
-        aspect_ratio = 1.0;
-        image_width = 600;
-        samples_per_pixel = 200;
-        background = color(0,0,0);
-        lookfrom = point3(278, 278, -800);
-        lookat = point3(278, 278, 0);
-        vfov = 40.0;
-        break;
+      world = cornell_box();
+      aspect_ratio = 1.0;
+      image_width = 600;
+      samples_per_pixel = 200;
+      background = color(0,0,0);
+      lookfrom = point3(278, 278, -800);
+      lookat = point3(278, 278, 0);
+      vfov = 40.0;
+      break;
 
     case 7:
       world = cornell_smoke();
