79e47689b8cd9db30d601b79b852aa53960af767
Wes <averagewizard13@gmail.com>
2021-07-26 09:18:05 -0600

    Replace lambertian box with glass ball

--- main.cpp
+++ main.cpp
@@ -71,10 +71,13 @@ hittable_list cornell_box() {
   box1 = make_shared<translate>(box1, vec3(265,0,295));
   objects.add(box1);
 
-  shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), white);
-  box2 = make_shared<rotate_y>(box2, -18);
-  box2 = make_shared<translate>(box2, vec3(130,0,65));
-  objects.add(box2);
+  // shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165,165,165), white);
+  // box2 = make_shared<rotate_y>(box2, -18);
+  // box2 = make_shared<translate>(box2, vec3(130,0,65));
+  // objects.add(box2);
+
+  auto glass = make_shared<dielectric>(1.5);
+  objects.add(make_shared<sphere>(point3(190,90,190), 90 , glass));
 
   return objects;
 }
